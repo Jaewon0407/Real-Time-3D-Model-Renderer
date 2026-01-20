a32ba30aa2b86478bc166dbee87b9c48d8348e10
Jaewon <jaewon.h4@gmail.com>
2026-01-19 22:21:33 -0800

    added imgui for UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
+
 #include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -10,6 +11,10 @@
 #include "../header/shader.h"
 #include "../header/mesh.h"
 #include "../header/renderer.h"
+
+#include "../extern/imgui/imgui.h"
+#include "../extern/imgui/backends/imgui_impl_glfw.h"
+#include "../extern/imgui/backends/imgui_impl_opengl3.h"
 
 float g_cameraRadius = 5.0f;
 
@@ -32,8 +37,20 @@ int main() {
     double horizontalAngle = 0.0f;
     double verticleAngle = 0.0f;
 
+    // ImGui initialization
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window.getHandle(), true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Model transform state
+    glm::vec3 modelTranslation(0.0f, 0.0f, 0.0f);
+    glm::vec3 modelRotation(0.0f, 0.0f, 0.0f); // Euler angles in degrees
+
+
     while (!window.shouldClose()) {
-        
         // zoom in and out
         double cursorX, cursorY;
         glfwGetCursorPos(window.getHandle(), &cursorX, &cursorY);
@@ -58,13 +75,23 @@ int main() {
             verticleAngle -= 0.005f;
         }
 
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Model Controls");
+        ImGui::SliderFloat3("Translate", (float*)&modelTranslation, -10.0f, 10.0f);
+        ImGui::SliderFloat3("Rotate (deg)", (float*)&modelRotation, -180.0f, 180.0f);
+        ImGui::End();
+
         int width, height;
         window.getFramebufferSize(width, height);
         float aspect = (float)width / (float)height;
         glViewport(0, 0, width, height);
-        
+
         renderer.clear(0.02f, 0.02f, 0.08f, 1.0f);
-        
+
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 100.0f);
         glm::vec3 cameraPos(
             g_cameraRadius * cos(verticleAngle) * sin(horizontalAngle),
@@ -73,13 +100,28 @@ int main() {
         );
         glm::vec3 target(0.0f, 0.0f, 0.0f);
         glm::vec3 up(0.0f, 1.0f, 0.0f);
-        glm::mat4 view = glm::lookAt(cameraPos, target, up);        
+        glm::mat4 view = glm::lookAt(cameraPos, target, up);
+
+        // Compose model matrix from ImGui controls
         glm::mat4 model = glm::mat4(1.0f);
- 
+        model = glm::translate(model, modelTranslation);
+        model = glm::rotate(model, glm::radians(modelRotation.x), glm::vec3(1,0,0));
+        model = glm::rotate(model, glm::radians(modelRotation.y), glm::vec3(0,1,0));
+        model = glm::rotate(model, glm::radians(modelRotation.z), glm::vec3(0,0,1));
+
         renderer.render(mesh, shader, model, view, projection, cameraPos);
+
+        // Render ImGui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         window.swapBuffers();
         window.pollEvents();
     }
-    
+
+    // Cleanup ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     return 0;
 }
